3d6f6e7f137f543a7a9aa5cd9dd5f54d9df781fd
Levente Lukacs <25IMC11565@fh-krems.ac.at>
2026-06-22 11:31:11 +0200

    set up config.py

---
+++ packages/template_package/template_package/config.py
@@ -0,0 +1,9 @@
+# get urls
+TRIP_UPDATES_URL = "https://go.bkk.hu/api/query/v1/ws/gtfs-rt/full/TripUpdates.pb"
+GTFS_ZIP_URL = "https://go.bkk.hu/api/static/v1/public-gtfs/budapest_gtfs.zip"
+
+# define delay category thresholds (TODO clarify! open to redifne)
+# values are in seconds
+ON_TIME_MAX = 120 # within 2 minutes = on_time
+MINOR_MAX = 300 # between 2 and 5 = minor_delay
+                # more than 5 = major_delay
